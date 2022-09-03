0c985ad559444e6812bc33f36c95948260f55d35
prabath udakandage <probes1234@hotmail.com>
2022-09-03 19:15:57 +1000

    efcore db connection

---
+++ market-watcher/Models/Models/TradingSite.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Models.Models
+{
+    public partial class TradingSite
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Url { get; set; }
+    }
+}
